88aaf7d324f59895ce00bceba585854382d8dd50
Vladislav <patch.code2002@gmail.com>
2023-02-24 23:01:59 +0300

    Some experiments with arguments passed from terminal

--- Task_0_helloworld/helloworld.c
+++ Task_0_helloworld/helloworld.c
@@ -2,11 +2,17 @@
 #include <stdio.h>
 int main (int argc, char *argv [])
 {
+	for (int i = 0; i < argc; i++)
+	{
+		printf ("%s\n", argv [i]);
+	}
+
 	int commsize, my_rank;
+	printf ("argc before calling MPI_Init() = %d\n", argc);
 	MPI_Init (&argc, &argv);
 	MPI_Comm_size (MPI_COMM_WORLD, &commsize);
 	MPI_Comm_rank (MPI_COMM_WORLD, &my_rank);
 	printf ("Communicator size = %d My rank= %d\n", commsize, my_rank);
-	printf ("argc = %d", argc);
+	printf ("argc after calling MPI_Init() = %d\n", argc);
 	MPI_Finalize ();
 }
