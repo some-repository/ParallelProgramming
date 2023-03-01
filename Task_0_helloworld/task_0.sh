f07afb7930ae61966c3093655b008a68127504ba
some-repository <patch.code2002@gmail.com>
2023-03-01 20:09:55 +0300

    Queue works with 8 cores

--- Task_0_helloworld/task_0.sh
+++ Task_0_helloworld/task_0.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#PBS -l walltime=00:01:00,nodes=7:ppn=1
+#PBS -l walltime=00:01:00,nodes=2:ppn=4
 #PBS -N helloworld
 #PBS -q batch
 
 cd $PBS_O_WORKDIR
-mpirun --hostfile $PBS_NODEFILE -np 3 ./helloworld
+mpirun --hostfile $PBS_NODEFILE -np 8 ./helloworld
