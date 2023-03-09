b6d33b90b1749d5fe12e4b8b4bf1b8f27b2af195
Vladislav <patch.code2002@gmail.com>
2023-03-09 14:13:42 +0300

    1st version of task 2

---
+++ Task_2/main.c
@@ -0,0 +1,72 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv [])
+{
+	MPI_Init (&argc, &argv);
+
+	int commsize, my_rank;
+	MPI_Status status;
+	MPI_Comm_size (MPI_COMM_WORLD, &commsize);
+	MPI_Comm_rank (MPI_COMM_WORLD, &my_rank);
+
+	int N = atoi (argv [0]);
+	unsigned int epp = 0;
+
+	if (my_rank == 0)
+	{
+		printf ("N = %d\n", N);
+
+		if (N % commsize == 0)
+		{
+			epp = N / commsize; // elements per process
+		}
+		else
+		{
+			epp = (N / commsize) + 1; // elements per process
+		}
+
+		for (int i = 1; i < commsize; i++)
+		{
+			MPI_Send (&epp, 1, MPI_UNSIGNED, i, MPI_ANY_TAG, MPI_COMM_WORLD);
+		}
+
+		double sum = sum_elements (my_rank, epp, N);
+
+		double tmp = 0;
+		for (int i = 1; i < commsize; i++)
+		{
+			MPI_Recv (&tmp, 1, MPI_DOUBLE, i, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+			sum += tmp;
+		}
+
+		printf ("sum = %f", sum);
+	}
+	else
+	{
+		MPI_Recv (&epp, 1, MPI_UNSIGNED, i, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+
+		double sum = sum_elements (my_rank, epp, N);
+
+		MPI_Send (&sum, 1, MPI_DOUBLE, i, MPI_ANY_TAG, MPI_COMM_WORLD);
+	}
+
+	MPI_Finalize ();
+	return 0;
+}
+
+double sum_elements (int my_rank, unsigned int epp, int N)
+{
+	double sum = 0;
+	for (int i = my_rank * epp + 1; i < (my_rank + 1) * epp + 1; i++)
+	{
+		if (i > N)
+		{
+			break;
+		}
+		sum += 1 / i;
+		printf ("My rank = %d, i = %d\n", my_rank, i);
+	}
+	return sum;
+}
